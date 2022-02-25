eb21108b133c76314874da47cb8ff5c840835cc3
Nayunik <koryukin.danil@list.ru>
2022-02-26 04:47:56 +0500

    Добавление сервисов

--- Startup.cs
+++ Startup.cs
@@ -1,3 +1,6 @@
+using FirstWeb.Data.Interfaces;
+using FirstWeb.Data.mo;
+using FirstWeb.Data.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -8,6 +11,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+
 
 namespace FirstWeb
 {
@@ -23,6 +27,8 @@ namespace FirstWeb
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddTransient<IAllCars, MoCars>();
+            services.AddTransient<ICarsCategory, moCategory>();
             services.AddRazorPages();
             services.AddMvc();
             
